7b62b3d0a132f80fe2e6e98941a2bcba5bf14b29
Oguzcan Aygun <aygunoguzcan@outlook.com>
2023-07-26 15:29:41 +0300

    2

--- main.js
+++ main.js
@@ -21,9 +21,50 @@ class Block {
 
 class Blockchain{
     constructor(){
-        this.chain = this.createGenesisBlock();
+        this.chain = [this.createGenesisBlock()];
     }
     createGenesisBlock(){
-        return new Block(0, 26/07/2023, "Genesis Block", "0");
+        return new Block(0, "26/07/2023", "Genesis Block", "0");
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+
+    isChainValid(){
+        for (let i = 1; i < this.chain.length; i++)
+        {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i-1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash())
+            {
+                return false;
+            }
+
+            if (currentBlock.previousHash !== previousBlock.hash)
+            {
+                return false;
+            }
+        }
+        
+        return true;
+
     }
 }
+
+let oguzCoin = new Blockchain();
+oguzCoin.addBlock(new Block(1, "27/07/2023", { info: "test1"}));
+oguzCoin.addBlock(new Block(2, "30/07/2023", { info: "test2"}));
+
+// oguzCoin : object to be transformed into JSONstring
+// null : optional
+// 4 : number of spaces between console.log
+console.log("Is Blockchain Valid ? : " + oguzCoin.isChainValid());
+console.log(JSON.stringify(oguzCoin, null, 4));
