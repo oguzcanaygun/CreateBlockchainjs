22508778fb3c66d52d90c5837ab23a2e7374b8ff
Oguzcan Aygun <aygunoguzcan@outlook.com>
2023-07-27 16:08:14 +0300

    implement POW

--- main.js
+++ main.js
@@ -13,15 +13,26 @@ class Block {
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
     calculateHash(){
-        return SHA256(this.index + this.timestamp + JSON.stringify(this.data) + this.previousHash).toString();
+        return SHA256(this.index + this.timestamp + JSON.stringify(this.data) + this.previousHash + this.nonce).toString();
+    }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log("Block mined: " + this.hash);
     }
 }
 
 class Blockchain{
     constructor(){
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 5;
     }
     createGenesisBlock(){
         return new Block(0, "26/07/2023", "Genesis Block", "0");
@@ -33,7 +44,7 @@ class Blockchain{
 
     addBlock(newBlock){
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
 
@@ -60,11 +71,18 @@ class Blockchain{
 }
 
 let oguzCoin = new Blockchain();
+
+console.log("mining block 1 ...");
+
 oguzCoin.addBlock(new Block(1, "27/07/2023", { info: "test1"}));
+
+console.log("mining block 2 ...");
+
 oguzCoin.addBlock(new Block(2, "30/07/2023", { info: "test2"}));
 
 // oguzCoin : object to be transformed into JSONstring
 // null : optional
 // 4 : number of spaces between console.log
-console.log("Is Blockchain Valid ? : " + oguzCoin.isChainValid());
-console.log(JSON.stringify(oguzCoin, null, 4));
+//console.log("Is Blockchain Valid ? : " + oguzCoin.isChainValid());
+//console.log(JSON.stringify(oguzCoin, null, 4));
+
